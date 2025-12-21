90d491331e014333fcb9f25ded5cefc232f11dda
Mohammed El Shora <riyadm2001@gmail.com>
2025-12-21 16:10:55 +0200

    change in search box by adding icon notification

--- lib/custom/home_search_box.dart
+++ lib/custom/home_search_box.dart
@@ -1,5 +1,15 @@
 import 'package:active_ecommerce_cms_demo_app/app_config.dart';
+import 'package:active_ecommerce_cms_demo_app/custom/toast_component.dart';
+import 'package:active_ecommerce_cms_demo_app/helpers/shared_value_helper.dart';
+
+import 'package:active_ecommerce_cms_demo_app/presenter/unRead_notification_counter.dart';
+import 'package:active_ecommerce_cms_demo_app/screens/notification/notification_list.dart';
+import 'package:active_ecommerce_cms_demo_app/screens/wishlist/widgets/page_animation.dart';
+
 import 'package:flutter/material.dart';
+import 'package:badges/badges.dart' as badges;
+import 'package:provider/provider.dart';
+import 'package:active_ecommerce_cms_demo_app/locale/custom_localization.dart';
 
 class HomeSearchBox extends StatelessWidget {
   final BuildContext? context;
@@ -37,6 +47,56 @@ class HomeSearchBox extends StatelessWidget {
             Text(
               AppConfig.search_bar_text(context),
               style: const TextStyle(fontSize: 13.0, color: Color(0xff7B7980)),
+            ),
+            const Spacer(),
+            badges.Badge(
+              showBadge: is_logged_in.$,
+              position: badges.BadgePosition.topEnd(top: 0, end: 0),
+              badgeStyle: badges.BadgeStyle(
+                shape: badges.BadgeShape.circle,
+                badgeColor: Theme.of(context).primaryColor,
+                borderRadius: BorderRadius.circular(AppDimensions.radiusNormal),
+                padding: const EdgeInsets.all(3),
+              ),
+              ignorePointer: true,
+              badgeContent: Consumer<UnReadNotificationCounter>(
+                builder: (context, notification, child) {
+                  return Text(
+                    "${notification.unReadNotificationCounter}",
+                    style: const TextStyle(fontSize: 9, color: Colors.white),
+                  );
+                },
+              ),
+              child: GestureDetector(
+                onTap: is_logged_in.$
+                    ? () {
+                        Navigator.push(
+                                context,
+                                PageAnimation.fadeRoute(
+                                    const NotificationList()))
+                            .then((value) {
+                          Provider.of<UnReadNotificationCounter>(context,
+                                  listen: false)
+                              .getCount();
+                        });
+                      }
+                    : () {
+                        ToastComponent.showDialog(
+                          'you_need_to_log_in'.tr(context: context),
+                          isError: true,
+                        );
+                      },
+                child: Padding(
+                  padding: const EdgeInsets.all(5.0),
+                  child: Image.asset(
+                    AppImages.notification,
+                    height: 18,
+                    color: is_logged_in.$
+                        ? const Color(0xff7B7980)
+                        : const Color(0xff7B7980),
+                  ),
+                ),
+              ),
             ),
           ],
         ),
